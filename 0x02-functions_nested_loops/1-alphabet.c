c38a83c6e5cbf083feafd1adab1620f329ec4bfb
Ibrahim170 <hemayassen@gmail.com>
2023-02-21 18:19:12 -0800

    add _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Main
+ * Description: print alphabet , lowercase
+ *
+ * Return: void.
+ */
+void print_alphabet(void)
+{
+	int chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		_putchar(chr);
+	}
+	_putchar(chr);
+}
