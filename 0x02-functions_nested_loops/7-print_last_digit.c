ee51fd666eee7b87f1840b8bc06cace6c2bd8541
Ibrahim170 <hemayassen@gmail.com>
2023-02-21 19:10:22 -0800

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,6 +7,22 @@
  * Return: integer
  */
 
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
 int print_last_digit(int n)
 {
 	int l;
